e500e8a1bc132a3c0d3e99b76ee09fcba8ca394d
James <IdiosApps@users.noreply.github.com>
2019-01-05 00:40:24 +0000

    Check for internet access before checking for phone's ID

--- autoAndroidUSBTether.ps1
+++ autoAndroidUSBTether.ps1
@@ -10,16 +10,17 @@ do { # huge do-while loop to keep USB detection alive
     if ($eventType -eq 1){
         # a USB device has been inserted - let's see if it's our Android device
         timeout -t 5 # give adb daemon a little time to initialise
-        $pluggedInCheck = Invoke-Expression "cmd /c adb devices | out-string"
-        if ($pluggedInCheck.Contains('ADB_DEVICE_CODE')) { ### USE YOUR DEVICE'S ADB HERE (run "adb devices" in a command prompt to get this - requires adb drivers)
-            # when phone is plugged in, check if we have internet
-            $pingTest = Invoke-Expression "cmd /c ping 1.1.1.1 -n 1 | out-string"
-            if ($pingTest.Contains("PING: transmit failed")) {
+        # when phone is plugged in, check if we have internet
+        $pingTest = Invoke-Expression "cmd /c ping 1.1.1.1 -n 1 | out-string"
+        if ($pingTest.Contains("PING: transmit failed")) {
+            $pluggedInCheck = Invoke-Expression "cmd /c adb devices | out-string"
+            if ($pluggedInCheck.Contains('ADB_DEVICE_CODE')) { ### USE YOUR DEVICE'S ADB HERE (run "adb devices" in a command prompt to get this - requires adb drivers)
+
                 # we don't have internet, so let's enable USB tethering
                 # first, let's unlock
                 adb shell input keyevent KEYCODE_WAKEUP
                 adb shell input touchscreen swipe 1 479 1 0
-                adb shell input text ANDROID_PIN_CODE ### CHANGE THIS TO YOUR PHONE'S PIN
+                adb shell input text ANDROID_PIN_CODE  ### CHANGE THIS TO YOUR PHONE'S PIN
                 adb shell input keyevent 66
                 adb shell am force-stop com.android.settings
 
